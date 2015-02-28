d29341c6c7cb1e7752b7d219be55b9d7fcb2204f
kelvinst <kelvin.stinghen@gmail.com>
2015-02-28 11:23:06 -0300

    A class on my js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 $('button').click(function() {
   console.log('clicked!');
-})
+});
+
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype.toString = function() {
+  return this.name;
+}
+
+window.animals = [];
+window.animalIndex = 0;
+
+$('.new-animal').click(function() {
+  window.animals.push(new Animal("animal" + window.animalIndex));
+  window.animalIndex++;
+});
